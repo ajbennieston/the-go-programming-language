eee90d75bd2a359f71518df377bf9b8cc9ccdb74
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2016-09-20 20:53:59 +0100

    Exercise 2.1: Modify tempconv to handle Kelvin temperatures.

--- ch2/src/gopl.io/ch2/tempconv/tempconv.go
+++ ch2/src/gopl.io/ch2/tempconv/tempconv.go
@@ -4,14 +4,21 @@ package tempconv
 
 import "fmt"
 
+type Kelvin float64
 type Celsius float64
 type Fahrenheit float64
 
 const (
+    AbsoluteZeroK Kelvin = 0
     AbsoluteZeroC Celsius = -273.15
     FreezingC Celsius = 0
     BoilingC Celsius = 100
+    FreezingF Fahrenheit = 32
 )
+
+func (k Kelvin) String() string {
+    return fmt.Sprintf("%gK", k)
+}
 
 func (c Celsius) String() string {
     return fmt.Sprintf("%g°C", c)
--- ch2/test-tempconv.go
+++ ch2/test-tempconv.go
@@ -8,5 +8,17 @@ import (
 )
 
 func main() {
-    fmt.Printf("Absolute zero is %v, or %v\n", tempconv.AbsoluteZeroC, tempconv.CToF(tempconv.AbsoluteZeroC))
+    fmt.Printf("Absolute zero is %v, or %v, or %v (%v, %v)\n",
+        tempconv.AbsoluteZeroC,
+        tempconv.CToF(tempconv.AbsoluteZeroC),
+        tempconv.CToK(tempconv.AbsoluteZeroC),
+        tempconv.KToF(tempconv.AbsoluteZeroK),
+        tempconv.KToC(tempconv.AbsoluteZeroK))
+    fmt.Printf("Freezing point is %v, or %v, or %v (%v, %v, %v)\n",
+        tempconv.FreezingC,
+        tempconv.CToF(tempconv.FreezingC),
+        tempconv.CToK(tempconv.FreezingC),
+        tempconv.KToF(tempconv.Kelvin(273.15)),
+        tempconv.KToC(tempconv.Kelvin(273.15)),
+        tempconv.FToK(tempconv.FreezingF))
 }
