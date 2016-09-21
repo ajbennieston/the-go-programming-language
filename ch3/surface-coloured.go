8c17260f7604ed6b40f50fd47b94e9ccb318fa30
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2016-09-21 18:16:59 +0100

    Significantly refactor surface-coloured.go.
    * Add Canvas struct to hold canvas data.
    * Move most of the logic around to make it easier to turn into a package
      at a later date.
    * Rename some of the functions.
    * Keep state locally.
    * Have surfacePlot() accept an io.Writer parameter to make it easier to
      change where the output goes.

--- ch3/surface-coloured.go
+++ ch3/surface-coloured.go
@@ -4,19 +4,45 @@ package main
 
 import (
     "fmt"
+    "io"
     "math"
+    "os"
 )
 
-const (
-    width, height = 600, 320            // canvas size in pixels
-    cells         = 100                 // number of grid cells
-    xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
-    xyscale       = width / 2 / xyrange // pixels per x or y unit
-    zscale        = height * 0.4        // pixels per z unit
-    angle         = math.Pi / 6         // angle of x and y axes (30˚)
-)
+func main() {
+    width := 600
+    height := 320
+    cells := 100
+    xyrange := 30.0
+    angle := math.Pi / 6.0
 
-var palette = [...]string {
+    canvas := makeCanvas(width, height, cells, xyrange, angle)
+    surfacePlot(canvas, os.Stdout)
+}
+
+// Canvas properties
+type Canvas struct {
+    width    int
+    height   int
+    cells    int
+    xyrange  float64
+    angle    float64
+
+    xyscale  float64
+    zscale   float64
+    cosAngle float64
+    sinAngle float64
+}
+
+func makeCanvas(width int, height int, cells int, xyrange float64, angle float64) Canvas {
+    xyscale := float64(width) / 2 / xyrange
+    zscale := float64(height) * 0.4
+    cosAngle := math.Cos(angle)
+    sinAngle := math.Sin(angle)
+    return Canvas{width, height, cells, xyrange, angle, xyscale, zscale, cosAngle, sinAngle}
+}
+
+var palette = [...]string { // blue .. red
     "#0019bf",
     "#1616ac",
     "#2d1499",
@@ -29,19 +55,21 @@ var palette = [...]string {
     "#ce0215",
     "#e50003"}
 
-func color_map(height, min, max float64) string {
-    x := (float64(len(palette))/(max - min)) * (height - min)
-    return palette[int(x)]
+func colorMap(z, min, max float64) string {
+    // Map one numeric range onto another:
+    idx := (float64(len(palette))/(max - min)) * (z - min)
+    // Truncate and use as index in palette:
+    return palette[int(idx)]
 }
 
-var sin30, cos30 = math.Sin(angle), math.Cos(angle)
-
+// A single point. (x, y) are the canvas coordinates, z is the function height.
 type Point struct {
     x float64
     y float64
     z float64
 }
 
+// A polygon defined by four points.
 type Polygon struct {
     a Point
     b Point
@@ -49,36 +77,38 @@ type Polygon struct {
     d Point
 }
 
-func main() {
-    fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' " +
+func surfacePlot(canvas Canvas, out io.Writer) {
+    // Populate canvas data
+    fmt.Fprintf(out,
+               "<svg xmlns='http://www.w3.org/2000/svg' " +
                "style='stroke: grey; fill: white; stroke-width: 0.7' " +
-               "width='%d' height='%d'>", width, height)
+               "width='%d' height='%d'>", canvas.width, canvas.height)
 
     var polygons []Polygon
     zmin := 0.0
     zmax := 0.0
 
-    for i := 0; i < cells; i++ {
-        for j := 0; j < cells; j++ {
-            a, err := corner(i + 1, j)
+    for i := 0; i < canvas.cells; i++ {
+        for j := 0; j < canvas.cells; j++ {
+            a, err := corner(i + 1, j, canvas)
             if err {
                 continue
             }
             zmin = math.Min(zmin, a.z)
             zmax = math.Max(zmax, a.z)
-            b, err := corner(i, j)
+            b, err := corner(i, j, canvas)
             if err {
                 continue
             }
             zmin = math.Min(zmin, b.z)
             zmax = math.Max(zmax, b.z)
-            c, err := corner(i, j + 1)
+            c, err := corner(i, j + 1, canvas)
             if err {
                 continue
             }
             zmin = math.Min(zmin, c.z)
             zmax = math.Max(zmax, c.z)
-            d, err := corner(i + 1, j + 1)
+            d, err := corner(i + 1, j + 1, canvas)
             if err {
                 continue
             }
@@ -87,23 +117,26 @@ func main() {
             polygons = append(polygons, Polygon{a, b, c, d})
         }
     }
+
     for _, p := range polygons {
-        mean_height := (p.a.z + p.b.z + p.c.z + p.d.z) / 4.0
-        color := color_map(mean_height, zmin, zmax) 
-        fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
+        meanHeight := (p.a.z + p.b.z + p.c.z + p.d.z) / 4.0
+        color := colorMap(meanHeight, zmin, zmax)
+        fmt.Fprintf(out,
+                   "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
                    p.a.x, p.a.y,
                    p.b.x, p.b.y,
                    p.c.x, p.c.y,
                    p.d.x, p.d.y,
                    color)
     }
-    fmt.Printf("</svg>\n")
+
+    fmt.Fprintf(out, "</svg>\n")
 }
 
-func corner(i, j int) (Point, bool) {
+func corner(i, j int, canvas Canvas) (Point, bool) {
     // Find point (x, y) at corner of cell (i, j)
-    x := xyrange * (float64(i) / cells - 0.5)
-    y := xyrange * (float64(j) / cells - 0.5)
+    x := canvas.xyrange * (float64(i) / float64(canvas.cells) - 0.5)
+    y := canvas.xyrange * (float64(j) / float64(canvas.cells) - 0.5)
 
     // Compute surface height z
     z := f(x, y)
@@ -112,10 +145,9 @@ func corner(i, j int) (Point, bool) {
     }
 
     // Project (x, y, z) isometrically onto 2D SVG canvas (sx, sy)
-    sx := width / 2 + (x - y) * cos30 * xyscale
-    sy := height / 2 + (x + y) * sin30 * xyscale - z * zscale
+    sx := float64(canvas.width) / 2 + (x - y) * canvas.cosAngle * canvas.xyscale
+    sy := float64(canvas.height) / 2 + (x + y) * canvas.sinAngle * canvas.xyscale - z * canvas.zscale
     return Point{sx, sy, z}, false
-
 }
 
 func f(x,y float64) float64 {
