48e76accdb8650a411fe0cabcad098edb3cdfd82
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2016-10-02 15:32:01 +0100

    Exercise 4.8: Modify charcount to count Unicode categories.

--- ch4/charcount.go
+++ ch4/charcount.go
@@ -13,6 +13,7 @@ import (
 
 func main() {
     counts := make(map[rune]int) // counts of Unicode code points
+    categoryCounts := make(map[string]int) // counts of code points by Unicode category
     var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
     invalid := 0
 
@@ -32,6 +33,26 @@ func main() {
         }
         counts[r]++
         utflen[n]++
+        category := "unknown"
+        switch {
+        case unicode.IsControl(r):
+            category = "control"
+        case unicode.IsDigit(r):
+            category = "digit"
+        case unicode.IsLetter(r):
+            category = "letter"
+        case unicode.IsMark(r):
+            category = "mark"
+        case unicode.IsNumber(r):
+            category = "number"
+        case unicode.IsPunct(r):
+            category = "punctuation"
+        case unicode.IsSpace(r):
+            category = "space"
+        case unicode.IsSymbol(r):
+            category = "symbol"
+        }
+        categoryCounts[category]++
     }
 
     fmt.Printf("rune\tcount\n")
@@ -43,6 +64,10 @@ func main() {
     fmt.Printf("---\t-----\n")
     for i, n := range utflen {
         fmt.Printf("%d\t%4d\n", i, n)
+    }
+    fmt.Printf("\n")
+    for category, count := range categoryCounts {
+        fmt.Printf("%12s:\t%4d\n", category, count)
     }
     if invalid > 0 {
         fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
