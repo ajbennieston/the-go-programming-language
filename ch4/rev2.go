e2bbd0def74136838e5490c0dc3fa962a2afa4a6
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2016-10-02 10:53:42 +0100

    Exercise 4.3: Rewrite `rev` to use an array pointer.

---
+++ ch4/rev2.go
@@ -0,0 +1,18 @@
+// rev: Reverses an array of ints in place.
+
+package main
+
+import "fmt"
+
+func reverse (s *[5]int) {
+    for i, j := 0, len(*s) - 1; i < j; i, j = i + 1, j - 1 {
+        (*s)[i], (*s)[j] = (*s)[j], (*s)[i]
+    }
+}
+
+func main() {
+    a := [...]int{1, 2, 3, 4, 5}
+    fmt.Println(a)
+    reverse(&a)
+    fmt.Println(a)
+}
