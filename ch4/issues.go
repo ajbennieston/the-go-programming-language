040537f6c33fa0d9e486d8f43f4a40487a67021c
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2016-10-15 12:50:34 +0100

    Exercise 4.10: Group github issue search results by age.
    Determine when an issue was created and print the results of the search
    by age groupings for < 1 day, < 1 month, < 1 year and > 1 year.

--- ch4/issues.go
+++ ch4/issues.go
@@ -6,7 +6,14 @@ import (
     "fmt"
     "log"
     "os"
+    "time"
     "gopl.io/ch4/github"
+)
+
+const (
+    Day = 24 * time.Hour
+    Month = 30 * Day
+    Year = 365 * Day
 )
 
 func main() {
@@ -15,9 +22,34 @@ func main() {
         log.Fatal(err)
     }
 
-    fmt.Printf("%d issues:\n", result.TotalCount)
+    issueMap := make(map[string][]*github.Issue)
     for _, item := range result.Items {
-        fmt.Printf("#%-5d %9.9s %.55s\n",
-                   item.Number, item.User.Login, item.Title)
+        issueAge := time.Since(item.CreatedAt)
+        switch {
+        case issueAge < Day:
+            issueMap["less than a day"] = append(issueMap["less than a day"], item)
+        case issueAge < Month:
+            issueMap["less than a month"] = append(issueMap["less than a month"], item)
+        case issueAge < Year:
+            issueMap["less than a year"] = append(issueMap["less than a year"], item)
+        default:
+            issueMap["more than a year"] = append(issueMap["more than a year"], item)
+        }
+    }
+
+    ages := [...]string{"less than a day", "less than a month", "less than a year", "more than a year"}
+    for _, age := range ages {
+        printIssues(issueMap, age)
+    }
+}
+
+func printIssues(issueMap map[string][]*github.Issue, age string) {
+    if len(issueMap[age]) > 0 {
+        fmt.Printf("Issues %s old:\n", age)
+        for _, item := range issueMap[age] {
+            fmt.Printf("  #%-5d %9.9s %.55s\n",
+                       item.Number, item.User.Login, item.Title)
+        }
+        fmt.Println()
     }
 }
